7b80e14a33fa3059cbdabb5938bd305b7fb955e7
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-03-22 16:56:42 -0400

    added const to dom elematns

--- app.js
+++ app.js
@@ -3,3 +3,22 @@
 // initialize state
 
 // set event listeners to update state and DOM
+
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const input3 = document.getElementById('input3');
+const input4 = document.getElementById('input4');
+const input5 = document.getElementById('input5');
+const input6 = document.getElementById('input6');
+
+// console.log(input1, input2, input3, input4, input5, input6);
+
+const output1 = document.getElementById('output1');
+const output2 = document.getElementById('output2');
+const output3 = document.getElementById('output3');
+const output4 = document.getElementById('output4');
+const output5 = document.getElementById('output5');
+const output6 = document.getElementById('output6');
+
+const submitButton = document.getElementById('submit-button');
+
